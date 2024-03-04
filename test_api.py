d76d404f2f3883bcc0f31457a5e9110358a14325
Victor Giraldo <vgiraldo@stevens.edu>
2024-03-04 18:43:34 -0500

    mock api calls

--- test_api.py
+++ test_api.py
@@ -1,24 +1,24 @@
 import unittest
+from unittest.mock import MagicMock as Mock, patch
 from api import get_commits_count
 
 class TestInputs(unittest.TestCase):
 
-    def testInvalidInput(self):
-        self.assertEqual(get_commits_count(1), "Invalid input. User must be a string.")
-
-    def testInvalidInput(self):
-        self.assertEqual(get_commits_count("jgo$&@"), "Invalid input. Github usernames can only contain alphanumeric characters or -'s")
-        
-    def testGetRepos(self):
-        self.assertEqual(get_commits_count("richkempinski"), [('csp', 2), ('hellogitworld', 30), ('helloworld', 6), ('Mocks', 10), ('Project1', 2), ('richkempinski.github.io', 9), ('threads-of-life', 1), ('try_nbdev', 2), ('try_nbdev2', 5)])
-
-    def testGetReposCount(self):
-        self.assertEqual(len(get_commits_count("richkempinski")), 9)
-
-    def testGetCommitsCount(self):
+    @patch('api.requests.get')
+    def test_get_commits_count(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = [
+            {'name': 'repo1'},
+            {'name': 'repo2'}
+        ]
         commits_count = get_commits_count("richkempinski")
-        for repo, count in commits_count:
-            self.assertGreaterEqual(count, 0, f"Invalid commit count for repository {repo}")
+        self.assertEqual(commits_count, [('repo1', 2), ('repo2', 2)])
+
+    @patch('api.requests.get')
+    def test_get_commits_count_failed_repos(self, mock_get):
+        mock_get.return_value.status_code = 404
+        commits_count = get_commits_count("richkempinski")
+        self.assertEqual(commits_count, "Failed to retrieve repositories. Status code: 404")
 
 if __name__ == '__main__':
     unittest.main()
